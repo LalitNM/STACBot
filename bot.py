4b31f87eb8cfd3e21a53f4c3bb9d45dac7cc1135
gd-codes <45459312+gd-codes@users.noreply.github.com>
2022-02-15 09:56:08 +0530

    Added commands for new features

--- bot.py
+++ bot.py
@@ -1,14 +1,31 @@
 # bot.py
 import os
 import random
+import traceback
+import datetime
+import  asyncio
+from dotenv import load_dotenv
+
+import astroevents
 
 import discord
 
+
+load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 BOT_ID = os.getenv('BOT_ID')
+
 client = discord.Client()
-customEmojis = ['asteroid:788060704011845662', 'earth:788060705764540426', 'jupiter:788060702291656725', 'mars:788060701284499517', 'mercury:788060703315460106', 'moon:788060703235637268', 'neptune:788060701776019456', 'pluto:788060700739239967', 'saturn:788060701893460021', 'sun:788060705483915284', 'uranus:788060705102102610', 'venus:788060699694727178']
+
+customEmojis = ['asteroid:788060704011845662', 'earth:788060705764540426', 
+                'jupiter:788060702291656725', 'mars:788060701284499517', 
+                'mercury:788060703315460106', 'moon:788060703235637268', 
+                'neptune:788060701776019456', 'pluto:788060700739239967', 
+                'saturn:788060701893460021', 'sun:788060705483915284', 
+                'uranus:788060705102102610', 'venus:788060699694727178']
 fourEmojis = ['🤩', '🔥', '👍', '💯']
+
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
@@ -32,6 +49,7 @@ async def react(emojiList, message):
     for emoji in emojiList:
         await message.add_reaction(emoji)
     print('React emoji function finished')
+
 
 @client.event
 async def on_message(message):
@@ -66,11 +84,15 @@ async def on_message(message):
         if message.content.split()[1] == 'help':
             await message.channel.send('''
             Following Commands are allowed:
-            1. `@STACbot help`: replies with this help message.
-            2. `@STACbot react <n>th`: Reacts to <n>th message in history with '🤩', '🔥', '👍', '💯'. Default value of n is 1.
-            3. `@STACbot poll n`: starts a poll by reacting to n last messages with '👍' and '👎'.
-            4. This bot reacts with '🤩', '🔥', '👍', '💯' to attached images and if there is a link in a message.
-            ''')
+            1. `@{STACBot} help`: replies with this help message.
+            2. `@{STACBot} react [n:int,1+]`: Reacts to <n>th message in history with '🤩', '🔥', '👍', '💯'. Default value of n is 1.
+            3. `@{STACBot} poll <n:int,1+>`: starts a poll by reacting to n last messages with '👍' and '👎'.
+            4. `@{STACBot} events [month:1..12] [year:2002+]`: Get a list of astronomical events happening this month, or in the past
+            5. `@{STACBot} (picture|photo)` : Display an astronomy-related photo of the day.
+            This bot automatically reacts with '🤩', '🔥', '👍', '💯' to attached images and if there is a link in a message.
+            It also posts the upcoming events on the first day of each month in the dedicated channel.
+            '''.format(STACBot=client.user.name))
+            return
     except:
         print('This was not help command.')
     try:
@@ -89,7 +111,7 @@ async def on_message(message):
                 await react([content[-1]], message)
             return
     except:
-        print("\'react\' is not in message.")
+        print("\'react\' is not in message.\n", traceback.format_exc())
     print(message.content)
 
     try:
@@ -102,7 +124,80 @@ async def on_message(message):
             await message.delete()
             return
     except:
-        print("This was not a poll command.")
+        print("This was not a poll command.\n", traceback.format_exc())
+
+    try :
+        if content[1] == 'events':
+            print("Executing events command")
+            dt = None
+            if len(content) >= 3 and content[2].isdigit() :
+                try :
+                    if len(content) >= 4  and content[3].isdigit() :
+                        dt = datetime.date(int(content[3]), int(content[2]), 1)
+                    else :
+                        dt = datetime.date(datetime.date.today().year, int(content[2]), 1)
+                    if not datetime.date(2002,8,1) <= dt <= datetime.date.today() :
+                        raise ValueError
+                except ValueError :
+                    await message.channel.send("Invalid date", reference=message)
+                    return
+            imgdata, embdata = await astroevents.fetch_and_parse(dt, 
+                len(content)==4 and content[2:]==['with','photo'])
+            embed = discord.Embed.from_dict(embdata)
+            print("Embed length", len(embed))
+            await message.channel.send(embed=embed)
+            return
+    except:
+        print("Events command failed\n", traceback.format_exc())
+    
+    try :
+        if content[1]=='picture' or content[1]=='photo' :
+            print("Executing image of the day command")
+            imgdata = await astroevents.picoftheday()
+            url = imgdata.get('url_hd') or imgdata.get('url_regular', '')
+            embed = discord.Embed.from_dict({
+                'title':imgdata.get('title','Unknown'),
+                'description':"Image of the Day",
+                'url':url,
+                'image': {'url':url, 'height':imgdata.get('h'), 'width':imgdata.get('w')},
+                'footer': {'text':"astrobin.com/{hash}, taken by {author}".format(
+                    hash=imgdata.get('hash'), author=imgdata.get('user')
+                )}
+            })
+            await message.channel.send(embed=embed)
+            return
+    except :
+        print("Image command failed\n", traceback.format_exc())
+
+    # Bot was mentioned directly but none of the above commands matched
+    try :
+        await message.channel.send(f"Use `@{client.user.name} help` to see what I can do !", reference=message) # send as reply
+    except :
+        print(traceback.format_exc())
 
 
+
+async def loop_monthly():
+    await client.wait_until_ready()
+    channel = client.get_channel(id=int(os.getenv("EVENT_CHANNEL_ID")))
+    print("Beginning astro eventloop",str(datetime.datetime.now()))
+    while not client.is_closed():
+        # First day of the month
+        dt = datetime.datetime.now()
+        if dt.date == 1 :
+            print("Event fetch fired",str(datetime.datetime.now()))
+            try :
+                imgdata, embdata = await astroevents.fetch_and_parse()
+                print(imgdata, embdata, sep='\n\n', end='\n\n')
+                embed = discord.Embed.from_dict(embdata)
+                print("Embed length", len(embed))
+                await channel.send(embed=embed)
+            except :
+                print("ERROR\n", traceback.format_exc())
+        # Repeat every day
+        await asyncio.sleep(60 * 60 * 24)
+        print("Astro eventloop iteration",str(datetime.datetime.now()))
+
+
+client.loop.create_task(loop_monthly())
 client.run(TOKEN)
