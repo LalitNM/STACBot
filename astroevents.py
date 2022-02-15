36f5733c6d9f0cb3aeb736607d0a5db289662e5c
gd-codes <45459312+gd-codes@users.noreply.github.com>
2022-02-15 14:28:43 +0530

    Fixed timezone conversion bugs

--- astroevents.py
+++ astroevents.py
@@ -6,6 +6,8 @@ from markdownify import markdownify
 from dotenv import load_dotenv
 import datetime
 import traceback
+import functools
+import itertools
 import re
 import os
 import sys
@@ -20,11 +22,19 @@ IOTD_URL1 = "https://www.astrobin.com/api/v1/imageoftheday/?limit=1&api_key={key
 IOTD_URL2 = "https://www.astrobin.com{path}/?api_key={key}&api_secret={secret}&format=json"
 
 
-def convertz(yy,mm,d,h,m):
+def convertz(match,yy=None,mm=None,d=None,sub=True):
+    if len(match.groups())==2:
+        h, m = match.groups()
+    else :
+        h, m = match.groups()[0], 0
+    h,m = map(int, (h,m))
+    if type(d) != int : d = 1 # Placeholder, won't matter in this case
     offset = datetime.timedelta(hours=5.5)
     t0 = datetime.datetime(yy, mm, d, h, m) + offset
-    return (t0.month, t0.day, t0.hour, t0.minute)
-
+    if sub :
+        return f"{t0.hour:02}:{t0.minute:02}"
+    else :
+        return (t0.month, t0.day)
 
 
 async def picoftheday():
@@ -47,17 +57,10 @@ async def picoftheday():
             iotd_imagedetails = await resp2.json()
 
     return iotd_imagedetails
-    # return {'url_regular': 'https://www.astrobin.com/qer3uo/0/rawthumb/regular/','h':300,'w':500,'user':'x'}
-
 
 
 
 async def fetch_and_parse(dt=None, with_photo=True):
-
-    # return (
-    #     {'animated': False, 'bookmarks': 3, 'comments': 18, 'data_source': 'AMATEUR_HOSTING', 'dec': '-37.170', 'description': '', 'h': 1639, 'hash': 'yx76x9', 'id': 659781, 'imaging_cameras': ['FLI Proline 16803'], 'imaging_telescopes': ['Planewave 17" CDK'], 'is_final': False, 'is_solved': True, 'license': 0, 'license_name': 'ALL_RIGHTS_RESERVED', 'likes': 176, 'link': 'https://www.good-astronomy.com', 'link_to_fits': None, 'locations': [], 'orientation': '312.326', 'pixscale': '1.270', 'published': '2022-02-03T22:06:41.530320', 'ra': '50.655', 'radius': '0.419', 'remote_source': 'DSW', 'resource_uri': '/api/v1/image/659781/', 'revisions': [], 'solution_status': 'SUCCESS', 'subjects': ['Fornax A', 'Fornax B', 'NGC 1310', 'NGC 1316', 'NGC 1317'], 'title': 'NGC 1316', 'updated': '2022-02-14T05:52:03.630561', 'uploaded': '2022-01-29T13:15:31.667587', 'url_advanced_skyplot': None, 'url_advanced_skyplot_small': None, 'url_advanced_solution': None, 'url_duckduckgo': 'https://www.astrobin.com/yx76x9/0/rawthumb/duckduckgo/', 'url_duckduckgo_small': 'https://www.astrobin.com/yx76x9/0/rawthumb/duckduckgo_small/', 'url_gallery': 'https://www.astrobin.com/yx76x9/0/rawthumb/gallery/', 'url_hd': 'https://www.astrobin.com/yx76x9/0/rawthumb/hd/', 'url_histogram': 'https://www.astrobin.com/yx76x9/0/rawthumb/histogram/', 'url_real': 'https://www.astrobin.com/yx76x9/0/rawthumb/real/', 'url_regular': 'https://www.astrobin.com/yx76x9/0/rawthumb/regular/', 'url_skyplot': 'https://cdn.astrobin.com/images/skyplots/images/8588/2022/65bc5015-43bc-45b6-9679-050b2dc0bd2a.jpeg', 'url_solution': 'https://cdn.astrobin.com/solutions/images/8588/2022/65bc5015-43bc-45b6-9679-050b2dc0bd2a-1643469803.jpeg', 'url_thumb': 'https://www.astrobin.com/yx76x9/0/rawthumb/thumb/', 'user': 'crgood2', 'views': 462, 'w': 1716},
-    #     {'title': 'February 2022', 'description': 'Stargazing Events & Opportunities this month', 'url': 'https://skymaps.com/articles/n2202.html', 'color': 4416472, 'fields': [{'name': '\n\n> *1*', 'value': '**New Moon** at 11:18 IST. Start of lunation 1226.\n**Moon near Mercury** at 04:30 IST (24° from Sun, morning sky). Mag. −0.1.\n• [Mercury](https://en.wikipedia.org/wiki/Mercury_(planet)) (Wikipedia)'}, {'name': '\n\n> *3*', 'value': '**Moon near Jupiter** at 06:30 IST (evening sky). Mag. −2.0.\n• [Jupiter](http://en.wikipedia.org/wiki/Jupiter) (Wikipedia)'}, {'name': '\n\n> *5*', 'value': '**Saturn at conjunction** with the Sun at 00:30 IST. The ringed planet (not visible) passes into the morning sky.\n• [Saturn](http://en.wikipedia.org/wiki/Saturn) (Wikipedia)\n**Mars 0.2° from M22** (globular star cluster) at 14:30 IST (38° from Sun, morning sky). Mags. 1.4 and 5.2.'}, {'name': '\n\n> *8*', 'value': '**Moon near Uranus** (82° from Sun, evening sky) at 02:30 IST. Occultation visible from Antarctica. Mag. +5.8.\n**First Quarter Moon** at 19:21 IST.'}, {'name': '\n\n> *9*', 'value': '**Moon near the Pleiades** at 17:30 IST (evening sky).\n• [The Pleiades](http://en.wikipedia.org/wiki/Pleiades) (Wikipedia)\n**Venus at its brightest** at 19:30 IST. Mag. −4.7.\n• [Venus](http://en.wikipedia.org/wiki/Venus) (Wikipedia)'}, {'name': '\n\n> *10*', 'value': '**Moon near Aldebaran** at 12:30 IST (evening sky).\n• [Aldebaran](http://en.wikipedia.org/wiki/Aldebaran) (Wikipedia)'}, {'name': '\n\n> *11*', 'value': "**Moon at apogee** (farthest from Earth) at 08:30 IST (distance 404,897 km; angular size 29.5')."}, {'name': '\n\n> *12*', 'value': '**Moon near M35 star cluste**r at 10:30 IST (evening sky).'}, {'name': '\n\n> *13*', 'value': '**Venus shows greatest illuminated extent** (337 square arc seconds) at 03:30 IST. Mag. −4.6\n**Moon near Castor** at 23:30 IST (evening sky).'}, {'name': '\n\n> *14*', 'value': '**Moon near Pollux** at 05:30 IST (evening sky).'}, {'name': '\n\n> *15*', 'value': '**Moon near Beehive cluster M44** at 08:30 IST (evening sky).\n• [Beehive Cluster](http://en.wikipedia.org/wiki/Messier_44) (Wikipedia)  \n• [M44: The Beehive Cluster](http://apod.nasa.gov/apod/ap140222.html) (APOD)'}, {'name': '\n\n> *16*', 'value': '**Full Moon** at 22:28 IST.'}, {'name': '\n\n> *17*', 'value': '**Mercury at greatest elongation west** at 02:30 IST (26° from Sun, morning sky). Mag. 0.0.\n**Moon near Regulus** at 03:30 IST (midnight sky).\n• [Regulus](http://en.wikipedia.org/wiki/Regulus) (Wikipedia)'}, {'name': '\n\n> *21*', 'value': '**Moon near Spica** at 05:30 IST (morning sky).\n• [Spica](http://en.wikipedia.org/wiki/Spica) (Wikipedia)'}, {'name': '\n\n> *24*', 'value': '**Last Quarter Moon** at 04:03 IST.\n**Moon near Antares** at 13:30 IST (morning sky).\n• [Antares](http://en.wikipedia.org/wiki/Antares) (Wikipedia)'}, {'name': '\n\n> *27*', 'value': "**Moon at perigee** (closest to Earth) at 04:09 IST (distance 367,789km; angular size 32.5').\n**Moon near Venus** at 12:30 IST (47° from Sun, morning sky). Mag. −4.6.\n• [Venus](http://en.wikipedia.org/wiki/Venus) (Wikipedia)\n**Moon near Mars** at 16:30 IST (44° from Sun, morning sky). Mag. 1.3.\n• [Mars](http://en.wikipedia.org/wiki/Mars) (Wikipedia)"}], 'timestamp': '2022-02-14T06:03:48.633218', 'footer': {'text': 'skymaps.com/articles/n2202.html\nPicture of the Day from astrobin.com by crgood2'}, 'image': {'url': 'https://www.astrobin.com/yx76x9/0/rawthumb/regular/', 'height': 1639, 'width': 1716}}
-    # )
 
     global COLOURLIST, CALENDAR_URL
     DTNOW = dt or datetime.date.today()
@@ -76,32 +79,35 @@ async def fetch_and_parse(dt=None, with_photo=True):
     # Extract useful HTML, parse into markdown, convert timezones to custom value
     bs = BeautifulSoup(web_result, features="html.parser")
     events = {}
-    for tr in bs.select("table table table")[0]("tr"):
+    for tr in bs.select("table table")[0]("tr"):
         if len(tr('td')) != 2:
             continue
         try :
-            date = int(next(tr.td.font.stripped_strings))
+            DATE = int(next(tr.td.font.stripped_strings))
         except (ValueError, StopIteration):
             continue
         desc = ''.join(map(str, tr("td")[1].font.contents)).strip()
         neat = re.sub(r'\n+\s*\n*', '\n', markdownify(desc)).strip()
 
-        r1 = re.search(r'([0-9]{1,2})\:([0-9]{1,2})\s+(UT)', neat)
-        r2 = re.search(r'([0-9]{1,2})h\s+(UT)', neat)
-        if r1 :
-            h, m, ut = r1.groups()
-            mo, date, h, m = convertz(YEAR, MONTH, date, int(h), int(m))
-            neat = re.sub(r'([0-9]{1,2})\:([0-9]{1,2})\s+(UT)', f"{h:02}:{m:02} IST", neat)
-        elif r2 :
-            h, ut = r2.groups()
-            mo, date, h, m = convertz(YEAR, MONTH, date, int(h), 0)
-            neat = re.sub(r'([0-9]{1,2})h\s+(UT)', f"{h:02}:{m:02} IST", neat)
+        r1 = re.compile(r'\b([012]?[0-9])\:([0-5][0-9])\b')
+        r2 = re.compile(r'\b([012]?[0-9])h\b')
+        months, dates = [], []
+        # Does changing timezone shift the event into a diff date or month entirely ?
+        for match in itertools.chain(re.finditer(r1, neat), re.finditer(r2, neat)):
+            mo, dat = convertz(match, YEAR, MONTH, DATE, False)
+            dates.append(dat); months.append(mo)
+        if not any([m==MONTH for m in months]) : # Keep the event, but include month name with date
+            DATE = datetime.date(YEAR, months[0], dates[0]).strftime('%b %d')
+        elif not any([d==DATE for d in dates]) : # Shifted into neighbouring day
+            DATE = dates[0]
+        # Important to substitute r1 first, then r2
+        neat = re.sub(r1, functools.partial(convertz, yy=YEAR,mm=MONTH,d=DATE,sub=True), neat)
+        neat = re.sub(r2, functools.partial(convertz, yy=YEAR,mm=MONTH,d=DATE,sub=True), neat)
+        neat = re.sub(r'\bUT\b', 'IST', neat)
 
-        if mo != MONTH :
-            continue
-        if date not in events :
-            events[date] = []
-        events[date].append(neat)
+        if DATE not in events :
+            events[DATE] = []
+        events[DATE].append(neat)
     print(f"Parsed {len(events)} event fields")
 
 
